178682b4f62e18e1d3ba6e7b562155eb0657e4aa
Naumi003 <ansarinoman1203@gmail.com>
2023-10-16 19:39:21 +0530

    my task

---
+++ fitpeo tech pvt.ltd assignment/dashboard/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './header'
+import Sidebar from './assets/sidebar'
+import Home from './assets/home '
+import React from 'react'
+
+function App() {
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  }
+
+  return (
+    <div className='grid-container'>
+      <Header OpenSidebar={OpenSidebar}/>
+      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ fitpeo tech pvt.ltd assignment/dashboard/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import App from './App'
+// import header from './header'
+import ReactDOM from 'react-dom';
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    
+  </React.StrictMode>,
+)
